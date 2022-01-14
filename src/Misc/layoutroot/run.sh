ad59f5a23a147346a5880dec3a4a1ea4c38b2427
Ferenc Hammerl <fhammerl@github.com>
2022-01-14 13:02:47 +0100

    Add logging

--- src/Misc/layoutroot/run.sh
+++ src/Misc/layoutroot/run.sh
@@ -2,6 +2,7 @@
 
 # Change directory to the script root directory
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+echo 🔥🔥🔥 New runsh started
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
